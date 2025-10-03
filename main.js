3048f53c68fa3e26c7906d87ebab89aaba090e44
Jimmy34 <20060303jjc@gmail.com>
2025-10-04 06:10:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -67,8 +67,8 @@ function loadDataForDate(dateStr) {
       currentLayer = L.geoJSON(data, {
         style: feature => {
           const name = feature.properties.Name?.toLowerCase();
-          if (name === 'red') return { color: 'red', fillOpacity: 0.2, weight: 2 };
-          if (name === 'lib') return { color: 'blue', fillOpacity: 0.2, weight: 0 };
+          if (name === 'red') return { color: 'red', fillOpacity: 0.2, weight: 1.5 };
+          if (name === 'lib') return { color: 'blue', fillOpacity: 0.2, weight: 1.5 };
           if (name === 'contested') return { color: 'white', fillOpacity: 0.25, weight: 0 };
           if (name === 'dpr') return { color: 'purple', fillOpacity: 0.25, weight: 2 };
           return { color: 'black', fillOpacity: 0.3 };
