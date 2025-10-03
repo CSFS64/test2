0b50af3fe69c72b9181f69e325984223fc6120f0
Jimmy34 <20060303jjc@gmail.com>
2025-10-04 06:03:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -207,6 +207,7 @@ closeUpdatePanel.onclick = () => {
 
 // 📥 加载更新数据（你可以从 JSON 文件加载）
 const updates = [
+  { date: "2025-10-03", summary: "乌克兰武装部队在Boikivka方向取得了成功；更新了波克罗夫斯克（第聂伯彼得罗夫斯克州）方向的前线" },
   { date: "2025-09-14", summary: "乌克兰武装部队在Kindrativka（苏梅）附近推进；乌克兰武装部队清除了库普扬斯克的俄军；乌克兰武装部队解放了Pankivka（波克罗夫斯克）" },
   { date: "2025-09-07", summary: "澄清了库普扬斯克北部的情况；俄军向Zarichne推进；澄清了Katerynivka附近的情况；澄清了Yablunivka附近的情况；乌克兰武装部队解放了Volodimyrivka并向南部推进；澄清了Novotoreske附近的情况；俄军向利曼（波克罗夫斯克）推进；乌克兰武装部队向Razine方向推进" },
   { date: "2025-09-02", summary: "更新了波克罗夫斯克方向的交战区和解放区域" },
