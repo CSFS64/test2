be3074b20330c2aad322d49d1e54fea17cf4e627
Jimmy34 <20060303jjc@gmail.com>
2025-12-31 17:03:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ const MAP_NOTES_API = "https://map-api.20060303jjc.workers.dev"; // 你的 worke
 // popup 参数
 const NOTE_POPUP_OPTS = {
   className: "mn-popup",
-  maxWidth: 480,
+  minWidth: 480,
+  maxWidth: 900,
   autoPan: true
 };
 
